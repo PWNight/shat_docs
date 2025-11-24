0f9ae71da8821bc5ed59c7ccf73b28a2a231829d
PWNight <rodionvarzymov@outlook.com>
2025-11-24 17:03:32 +0300

    Добавил формы входа и регистрации

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,32 +1,90 @@
 "use client"
-import {useState} from "react";
-import {Loader2} from "lucide-react";
+import {useActionState, useEffect, useState} from "react";
+import {Loader2, UserPlusIcon} from "lucide-react";
+import Link from "next/link";
+import {useRouter} from "next/navigation";
+import {useSearchParams} from "next/navigation";
+
+function signup() {
+
+}
 
 export default function LoginPage() {
-    const pending = useState(false);
+    const [state, action, pending] = useActionState(signup, undefined);
+    const [showPassword, setShowPassword] = useState(false);
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const redirectTo = searchParams.get("to") || "/profile";
+
+    useEffect(() => {
+
+    }, [state, router, redirectTo]);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
     return (
-        <div className={'w-full h-screen flex items-center justify-center'}>
-            <form className={'shadow-xl bg-white p-8 flex flex-col gap-2 rounded-xl'}>
-                <h1 className={'text-4xl font-bold text-gray-900 mb-8 text-center'}>Вход в систему</h1>
-                <div className={'flex flex-col gap-2'}>
+        <div className="flex items-center justify-center h-screen">
+            <form
+                className="w-full max-w-md mx-auto bg-white rounded-2xl p-8 shadow-lg text-gray-900"
+                action={action}
+            >
+                <h1 className="text-3xl font-bold mb-4 select-none">Вход в систему</h1>
+                <div className="mb-4">
+                    <label htmlFor="username" className="block mb-1 font-medium text-sm">
+                        Почта
+                    </label>
                     <input
-                        className={'px-4 py-2 border rounded-lg focus:ring outline-none transition-all border-gray-600 placeholder-gray-600'}
-                        type="password"
-                        name={'password'}
-                        placeholder={'Введите пароль'}
-                        autoComplete={'password'}
+                        type="text"
+                        autoComplete="email"
+                        id="email"
+                        name="email"
+                        className="shadow-lg w-full px-4 py-3 bg-gray-50  border border-gray-200  rounded-lg focus:ring focus:ring-blue-400 focus:border-blue-400 outline-none transition-all placeholder-gray-400"
+                        placeholder="test@test.com"
                     />
-                    <input
-                        className={'px-4 py-2 border rounded-lg focus:ring outline-none transition-all border-gray-600 placeholder-gray-600'}
-                        type={'email'}
-                        name={'username'}
-                        placeholder={'Введите почту'}
-                        autoComplete={'email'}
-                    />
+                    {state?.errors?.username && (
+                        <p className="text-red-400 text-sm mt-2">{state.errors.username}</p>
+                    )}
+                </div>
+                <div className="mb-6">
+                    <label htmlFor="password" className="block mb-1 font-medium text-sm">
+                        Пароль
+                    </label>
+                    <div className="relative">
+                        <input
+                            type={showPassword ? "text" : "password"}
+                            autoComplete="current-password"
+                            id="password"
+                            name="password"
+                            className="shadow-lg w-full px-4 py-3 bg-gray-50 border border-gray-200 rounded-lg focus:ring focus:ring-blue-400 focus:border-blue-400 outline-none transition-all placeholder-gray-400"
+                            placeholder="********"
+                        />
+                        {state?.errors?.password && (
+                            <p className="text-red-400 text-sm mt-2">{state.errors.password}</p>
+                        )}
+                    </div>
+                    <div className="flex justify-between items-center mt-6 text-sm">
+                        <label className="flex items-center gap-2 select-none">
+                            <input
+                                id="show-password"
+                                type="checkbox"
+                                className="h-4 w-4 text-blue-400 focus:ring-blue-400 border-gray-300 rounded"
+                                onChange={togglePasswordVisibility}
+                            />
+                            Показать пароль
+                        </label>
+                        <Link
+                            href="https://t.me/pwnight"
+                            className="text-blue-400 hover:text-blue-500 transition-colors"
+                        >
+                            Забыли пароль?
+                        </Link>
+                    </div>
                 </div>
                 <button
                     type="submit"
-                    className="w-full bg-[#F38F54] hover:bg-orange-500 text-white font-medium py-3 px-5 rounded-lg focus:ring-4 focus:ring-orange-300 disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center gap-2 transition-all shadow-md hover:shadow-lg select-none"
+                    className="w-full bg-blue-400 hover:bg-blue-500 text-white font-medium py-3 px-5 rounded-lg focus:ring-2 focus:ring-blue-300 disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center gap-2 transition-all shadow-md hover:shadow-lg select-none"
                     disabled={pending}
                 >
                     {pending ? (
@@ -38,6 +96,18 @@ export default function LoginPage() {
                         "Войти"
                     )}
                 </button>
+                <div className="mt-6 text-sm">
+                    <p className="text-gray-600  flex items-center gap-2">
+                        Впервые здесь?
+                        <Link
+                            href="/register"
+                            className="text-blue-400 hover:text-blue-500 flex items-center gap-2 transition-colors"
+                        >
+                            Создать аккаунт
+                            <UserPlusIcon className="h-4 w-4" />
+                        </Link>
+                    </p>
+                </div>
             </form>
         </div>
     )
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,3 +1,130 @@
-export default function RegisterPage(){
+"use client"
+import {useActionState, useEffect, useState} from "react";
+import {Loader2, User, UserPlusIcon} from "lucide-react";
+import Link from "next/link";
+import {useRouter} from "next/navigation";
+import {useSearchParams} from "next/navigation";
 
+function signup() {
+
+}
+
+export default function RegisterPage() {
+    const [state, action, pending] = useActionState(signup, undefined);
+    const [showPassword, setShowPassword] = useState(false);
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const redirectTo = searchParams.get("to") || "/profile";
+
+    useEffect(() => {
+
+    }, [state, router, redirectTo]);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <form
+                className="w-full max-w-md mx-auto bg-white rounded-2xl p-8 shadow-lg text-gray-900"
+                action={action}
+            >
+                <h1 className="text-3xl font-bold mb-4 select-none">Регистрация в системе</h1>
+                <div className="mb-4">
+                    <label htmlFor="username" className="block mb-1 font-medium text-sm">
+                        Почта
+                    </label>
+                    <input
+                        type="text"
+                        autoComplete="email"
+                        id="email"
+                        name="email"
+                        className="shadow-lg w-full px-4 py-3 bg-gray-50  border border-gray-200  rounded-lg focus:ring focus:ring-blue-400 focus:border-blue-400 outline-none transition-all placeholder-gray-400"
+                        placeholder="test@test.com"
+                    />
+                    {state?.errors?.username && (
+                        <p className="text-red-400 text-sm mt-2">{state.errors.username}</p>
+                    )}
+                </div>
+                <div className="mb-4">
+                    <label htmlFor="password" className="block mb-1 font-medium text-sm">
+                        Пароль
+                    </label>
+                    <div className="relative">
+                        <input
+                            type={showPassword ? "text" : "password"}
+                            autoComplete="current-password"
+                            id="password"
+                            name="password"
+                            className="shadow-lg w-full px-4 py-3 bg-gray-50 border border-gray-200 rounded-lg focus:ring focus:ring-blue-400 focus:border-blue-400 outline-none transition-all placeholder-gray-400"
+                            placeholder="********"
+                        />
+                        {state?.errors?.password && (
+                            <p className="text-red-400 text-sm">{state.errors.password}</p>
+                        )}
+                    </div>
+                    <label htmlFor="password" className="block mt-4 mb-1 font-medium text-sm">
+                        Повтор пароля
+                    </label>
+                    <div className="relative">
+                        <input
+                            type={showPassword ? "text" : "password"}
+                            autoComplete="current-password"
+                            id="password"
+                            name="password"
+                            className="shadow-lg w-full px-4 py-3 bg-gray-50 border border-gray-200 rounded-lg focus:ring focus:ring-blue-400 focus:border-blue-400 outline-none transition-all placeholder-gray-400"
+                            placeholder="********"
+                        />
+                        {state?.errors?.password && (
+                            <p className="text-red-400 text-sm mt-2">{state.errors.password}</p>
+                        )}
+                    </div>
+                </div>
+                <div className="flex justify-between items-center mb-2 text-sm">
+                    <label className="flex items-center gap-2 select-none">
+                        <input
+                            id="show-password"
+                            type="checkbox"
+                            className="h-4 w-4 text-blue-400 focus:ring-blue-400 border-gray-300 rounded"
+                            onChange={togglePasswordVisibility}
+                        />
+                        Показать пароль
+                    </label>
+                    <Link
+                        href="https://t.me/pwnight"
+                        className="text-blue-400 hover:text-blue-500 transition-colors"
+                    >
+                        Забыли пароль?
+                    </Link>
+                </div>
+                <button
+                    type="submit"
+                    className="w-full bg-blue-400 hover:bg-blue-500 text-white font-medium py-3 px-5 rounded-lg focus:ring-2 focus:ring-blue-300 disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center gap-2 transition-all shadow-md hover:shadow-lg select-none"
+                    disabled={pending}
+                >
+                    {pending ? (
+                        <>
+                            <Loader2 className="h-5 w-5 animate-spin" />
+                            Регистрация...
+                        </>
+                    ) : (
+                        "Зарегистрироваться"
+                    )}
+                </button>
+                <div className="mt-6 text-sm">
+                    <p className="text-gray-600  flex items-center gap-2">
+                        Уже есть аккаунт?
+                        <Link
+                            href="/login"
+                            className="text-blue-400 hover:text-blue-500 flex items-center gap-2 transition-colors"
+                        >
+                            Войти
+                            <User className="h-4 w-4" />
+                        </Link>
+                    </p>
+                </div>
+            </form>
+        </div>
+    )
 }
