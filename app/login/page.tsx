57e778f209f92d627e9dc22cbe52a28012bf0ca0
PWNight <rodionvarzymov@outlook.com>
2025-11-25 15:22:22 +0300

    Добавил общую ошибку при входе и регистрации

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -79,6 +79,9 @@ export default function LoginPage() {
                         </Link>
                     </div>
                 </div>
+                {state?.message && (
+                    <p className="text-red-400 text-sm mb-2">{state.message}</p>
+                )}
                 <button
                     type="submit"
                     className="w-full bg-blue-400 hover:bg-blue-500 text-white font-medium py-3 px-5 rounded-lg focus:ring-2 focus:ring-blue-300 disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center gap-2 transition-all shadow-md hover:shadow-lg select-none"
