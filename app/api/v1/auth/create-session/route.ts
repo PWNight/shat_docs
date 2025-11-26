bb928cf1c57f1318e9871d26896ec939f5fb94f6
PWNight <rodionvarzymov@outlook.com>
2025-11-26 15:19:33 +0300

    Обновил API создания сессии

--- app/api/v1/auth/create-session/route.ts
+++ app/api/v1/auth/create-session/route.ts
@@ -1,36 +1,40 @@
-import { createSession, encrypt } from "@/utils/session";
-import { NextRequest, NextResponse } from "next/server";
+import { createSession } from "@/utils/session";
 import { SessionFormSchema } from "@/utils/definitions";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-    const data = await request.json();
-
-    // Проверяем полученные поля
-    const validatedFields = SessionFormSchema.safeParse(Object.fromEntries(data));
-    if (!validatedFields.success) {
-        return NextResponse.json({ success: false, message: "Отсутствуют некоторые параметры", errors: validatedFields.error.flatten().fieldErrors }, { status: 401 });
-    }
-
-    // Получаем данные из формы
-    const { uid, email } = validatedFields.data;
-
     try {
-        // Заносим UID и Почту в сессию
-        const expiresAt = new Date(Date.now() + 7  *  24  *  60  *  60  *  1000); // 7 дней
+        // Получаем JSON объект из формы
+        const data = await request.json();
 
-        const sessionToken = await encrypt({ data: { uid, email}, expiresAt });
+        // Проверяем данные формы
+        const validatedFields = SessionFormSchema.safeParse(data);
+        if (!validatedFields.success) {
+            return NextResponse.json(
+                {
+                    success: false,
+                    message: "Неверные данные",
+                    errors: validatedFields.error.flatten().fieldErrors,
+                },
+                { status: 400 }
+            );
+        }
 
-        await createSession(sessionToken, expiresAt)
-        return NextResponse.json({ success: true, token: sessionToken }, { status: 200 });
+        // Получаем данные
+        const { uid, email } = validatedFields.data;
 
+        // Создаём безопасную сессию
+        await createSession({ uid, email });
+
+        return NextResponse.json(
+            { success: true, message: "Сессия создана" },
+            { status: 200 }
+        );
     } catch (error: any) {
         return NextResponse.json({
             success: false,
             message: 'Серверная ошибка',
-            error: {
-                message: error.message,
-                code: error.code || 'UNKNOWN_ERROR'
-            }
-        }, {status:500})
+            error: error
+        }, { status: 500 });
     }
 }
