8d91f7d48213d41def39792fc3298a0c367495e2
PWNight <rodionvarzymov@outlook.com>
2025-11-26 15:20:42 +0300

    Перенёс обработку форм в отдельный файл

--- utils/functions.ts
+++ utils/functions.ts
@@ -1,12 +1,10 @@
 // Общий тип для API ответов с ошибкой
-import {LoginFormSchema, LoginFormState} from "@/utils/definitions";
-
 interface ApiErrorResponse {
     message?: string;
 }
 
 // Утилита для обработки API ответов
-async function handleApiResponse(response: Response) {
+export async function handleApiResponse(response: Response) {
     if (!response.ok) {
         const errorData: ApiErrorResponse = await response.json();
         console.log(errorData);
@@ -19,7 +17,7 @@ async function handleApiResponse(response: Response) {
 }
 
 // Утилита для авторизованных запросов
-async function makeAuthorizedRequest(url: string, token: string, data: object, method = 'GET') {
+export async function makeAuthorizedRequest(url: string, token: string, data: object, method = 'GET') {
     return fetch(url, {
         method,
         headers: {
@@ -28,41 +26,4 @@ async function makeAuthorizedRequest(url: string, token: string, data: object, m
         },
         body: JSON.stringify(data),
     });
-}
-
-// Утилита авторизации
-export async function Login(state: LoginFormState, formData: FormData) {
-    try {
-        // Проверяем полученные поля
-        const validatedFields = LoginFormSchema.safeParse(Object.fromEntries(formData));
-        if (!validatedFields.success) {
-            return { errors: validatedFields.error.flatten().fieldErrors };
-        }
-
-        // Получаем данные из формы
-        const { email, password } = validatedFields.data;
-        const redirectTo = formData.get("redirectTo")?.toString() || "/me";
-
-        // Отправляем POST запрос в авторизацию
-        const loginResponse = await fetch('/api/v1/auth/login', {
-            method: 'POST',
-            body: JSON.stringify({ email, password }),
-            headers: { 'Content-Type': 'application/json' }
-        });
-
-        // Получаем данные из ответа
-        const { data: id } = await handleApiResponse(loginResponse);
-
-        // Создаём сессию
-        await fetch('/api/v1/auth/create-session', {
-            method: 'POST',
-            body: JSON.stringify({ id, email }),
-            headers: { 'Content-Type': 'application/json' }
-        });
-
-        // Возвращаем успех и redirectTo
-        return { success: true, redirectTo };
-    } catch (error) {
-        return { message: error instanceof Error ? error.message : 'Произошла ошибка' };
-    }
 }
---
+++ utils/handlers.ts
@@ -0,0 +1,77 @@
+import { LoginFormSchema, LoginFormState } from "@/utils/definitions";
+import { handleApiResponse } from "@/utils/functions";
+
+// Код авторизации
+export async function Login(state: LoginFormState, formData: FormData) {
+    try {
+        // Проверяем полученные поля
+        const validatedFields = LoginFormSchema.safeParse(Object.fromEntries(formData));
+        if (!validatedFields.success) {
+            return { errors: validatedFields.error.flatten().fieldErrors };
+        }
+
+        // Получаем данные из формы
+        const { email, password } = validatedFields.data;
+        const redirectTo = formData.get("redirectTo")?.toString() || "/me";
+
+        // Отправляем POST запрос в авторизацию
+        const loginResponse = await fetch('/api/v1/auth/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        // Получаем данные из ответа
+        const { data: id } = await handleApiResponse(loginResponse);
+
+        // Создаём сессию
+        await fetch('/api/v1/auth/create-session', {
+            method: 'POST',
+            body: JSON.stringify({ id, email }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        // Возвращаем успех и redirectTo
+        return { success: true, redirectTo };
+    } catch (error) {
+        return { message: error instanceof Error ? error.message : 'Произошла ошибка' };
+    }
+}
+
+// Код авторизации
+export async function Register(state: LoginFormState, formData: FormData) {
+    try {
+        // Проверяем полученные поля
+        const validatedFields = LoginFormSchema.safeParse(Object.fromEntries(formData));
+        if (!validatedFields.success) {
+            return { errors: validatedFields.error.flatten().fieldErrors };
+        }
+
+        // Получаем данные из формы
+        const { email, password } = validatedFields.data;
+        const redirectTo = formData.get("redirectTo")?.toString() || "/me";
+
+        // Отправляем POST запрос в авторизацию
+        const loginResponse = await fetch('/api/v1/auth/register', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        // Получаем данные из ответа
+        const { data } = await handleApiResponse(loginResponse);
+
+        // Создаём сессию
+        await fetch('/api/v1/auth/create-session', {
+            method: 'POST',
+            body: JSON.stringify({ uid: data.uid, email }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        // Возвращаем успех и redirectTo
+        return { success: true, redirectTo };
+    } catch (error) {
+        console.log( error )
+        return { message: error instanceof Error ? error.message : 'Произошла ошибка' };
+    }
+}
