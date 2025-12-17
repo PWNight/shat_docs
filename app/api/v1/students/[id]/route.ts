58d3190f3224f7d6c18bd1239fc582bd546a0a0f
PWNight <rodionvarzymov@outlook.com>
2025-12-17 14:44:24 +0300

    Добавил заготовку для API студентов

---
+++ app/api/v1/students/[id]/route.ts
@@ -0,0 +1,148 @@
+import {NextRequest, NextResponse} from "next/server";
+import {execute, queryOne} from "@/utils/mysql";
+import {getSession} from "@/utils/session";
+import {z} from "zod";
+
+// GET STUDENT BY ID
+export async function GET(request: NextRequest, {params}: { params: Promise<{ id: string }> }) {
+    try{
+        const userData = await getSession();
+        if (!userData){
+            return NextResponse.json({ success: false, message: "Для доступа требуется авторизоваться" }, {status:401})
+        }
+
+        const {id} = await params;
+        const group = await queryOne(
+            'SELECT * FROM groups WHERE id = ?', [id]
+        );
+        if (!group) {
+            return NextResponse.json({ success: false, message: `Группа с айди ${id} не найдена` }, {status:404})
+        }
+        return NextResponse.json({ success: true, data: group }, {status:200})
+    } catch (error) {
+        console.error("Ошибка работы API", error);
+        const errorMessage =
+            error instanceof Error ? error.message : "Неизвестная ошибка сервера";
+
+        return NextResponse.json(
+            {
+                success: false,
+                message: "Внутренняя ошибка сервера",
+                error: {
+                    message: errorMessage,
+                    code: "SERVER_ERROR",
+                },
+            },
+            { status: 500 }
+        );
+    }
+}
+
+// UPDATE STUDENT BY ID
+export async function POST(request: NextRequest, {params}: { params: Promise<{ id: string }> }) {
+    try{
+        const userData = await getSession();
+        if (!userData){
+            return NextResponse.json({ success: false, message: "Для доступа требуется авторизоваться" }, {status:401})
+        }
+
+        const {id} = await params;
+        const student = await queryOne(
+            'SELECT * FROM students WHERE id = ?', [id]
+        );
+        if (!student) {
+            return NextResponse.json({ success: false, message: `Студент с айди ${id} не найден` }, {status:404})
+        }
+
+        // TODO: Написать проверку доступа к обновлению (обновить студента может только классный руководитель его группы либо администратор)
+
+        // Получаем JSON объект из формы
+        const data = await request.json();
+
+        // Проверяем полученные поля
+        const parsed = StudentFormSchema.safeParse(data);
+        if (!parsed.success) {
+            const tree = z.treeifyError(parsed.error);
+            return NextResponse.json(
+                {
+                    success: false,
+                    message: "Неверные данные",
+                    errors: tree,
+                },
+                { status: 400 }
+            );
+        }
+        
+        const updates: string[] = []
+        const values: string [] = []
+
+        for (const key in parsed.data) {
+            if (parsed.data.hasOwnProperty(key)) {
+                updates.push(`${key} = ?`);
+                values.push(parsed.data[key]);
+            }
+        }
+
+        if (updates.length === 0) {
+            return NextResponse.json({ success: false, message: "Данные из формы совпадают с данными в базе" }, { status: 400 });
+        }
+
+        values.push(id);
+        await execute(`UPDATE students SET ${updates.join(', ')} WHERE id = ?`, values)
+        return NextResponse.json({ success: true, message: "Студент успешно обновлён" }, { status: 200 });
+    } catch (error) {
+        console.error("Ошибка работы API", error);
+        const errorMessage =
+            error instanceof Error ? error.message : "Неизвестная ошибка сервера";
+
+        return NextResponse.json(
+            {
+                success: false,
+                message: "Внутренняя ошибка сервера",
+                error: {
+                    message: errorMessage,
+                    code: "SERVER_ERROR",
+                },
+            },
+            { status: 500 }
+        );
+    }
+}
+// DELETE STUDENT BY ID
+export async function DELETE(request: NextRequest, {params}: { params: Promise<{ id: string }> }) {
+    try{
+        const userData = await getSession();
+        if (!userData){
+            return NextResponse.json({ success: false, message: "Для доступа требуется авторизоваться" }, {status:401})
+        }
+
+        const {id} = await params;
+        const student = await queryOne(
+            'SELECT * FROM students WHERE id = ? LIMIT 1', [id]
+        );
+        if (!student) {
+            return NextResponse.json({ success: false, message: `Студент с айди ${id} не найден` }, {status:404})
+        }
+
+        // TODO: Написать проверку доступа к обновлению (обновить студента может только классный руководитель его группы либо администратор)
+
+        await execute("DELETE FROM students WHERE id = ?", [id]);
+        return NextResponse.json({ success: true, message: `Студент с айди ${id} успешно удален` }, {status:200})
+    } catch (error) {
+        console.error("Ошибка работы API", error);
+        const errorMessage =
+            error instanceof Error ? error.message : "Неизвестная ошибка сервера";
+
+        return NextResponse.json(
+            {
+                success: false,
+                message: "Внутренняя ошибка сервера",
+                error: {
+                    message: errorMessage,
+                    code: "SERVER_ERROR",
+                },
+            },
+            { status: 500 }
+        );
+    }
+}
---
+++ app/api/v1/students/route.ts
@@ -0,0 +1,101 @@
+import {NextRequest, NextResponse} from "next/server";
+import {execute, query, queryOne} from "@/utils/mysql";
+import {z} from "zod";
+import {getSession} from "@/utils/session";
+
+// GET ALL STUDENTS (FOR ADMINS)
+export async function GET(request: NextRequest) {
+    try{
+        const userData = await getSession();
+        if (!userData){
+            return NextResponse.json({ success: false, message: "Для доступа требуется авторизоваться" }, {status:401})
+        }
+
+        // TODO: Проверка доступа к просмотру (доступ только для администратора)
+
+        const students = await query(
+            'SELECT * FROM students',
+        );
+        return NextResponse.json({ success: true, data: students }, {status:200})
+    } catch (error) {
+        console.error("Ошибка работы API", error);
+        const errorMessage =
+            error instanceof Error ? error.message : "Неизвестная ошибка сервера";
+
+        return NextResponse.json(
+            {
+                success: false,
+                message: "Внутренняя ошибка сервера",
+                error: {
+                    message: errorMessage,
+                    code: "SERVER_ERROR",
+                },
+            },
+            { status: 500 }
+        );
+    }
+}
+
+// CREATE NEW STUDENT
+export async function POST(request: NextRequest) {
+    try {
+        const userData = await getSession();
+        if (!userData){
+            return NextResponse.json({ success: false, message: "Для доступа требуется авторизоваться" }, {status:401})
+        }
+
+        // Получаем JSON объект из формы
+        const data = await request.json();
+
+        // Проверяем полученные поля
+        const parsed = StudentFormSchema.safeParse(data);
+        if (!parsed.success) {
+            const tree = z.treeifyError(parsed.error);
+            return NextResponse.json(
+                {
+                    success: false,
+                    message: "Неверные данные",
+                    errors: tree,
+                },
+                { status: 400 }
+            );
+        }
+
+        // Получаем данные из формы
+        const { full_name, admission_year, fk_group } = parsed.data;
+
+        // Получаем группу из БД
+        const group = await queryOne(
+            'SELECT * FROM groups WHERE id = ? LIMIT 1',
+            [fk_group]
+        );
+        if (!group) {
+            return NextResponse.json({ success: false, message: `Группа с айди ${fk_group} не найдена` }, { status: 404 });
+        }
+
+        // TODO: Проверка доступа к добавлению студента (доступ только для классного руководителя)
+
+        await execute(
+            'INSERT INTO students (full_name, admission_year, fk_group) VALUES (?, ?)',
+            [full_name, admission_year, fk_group]
+        );
+
+        return NextResponse.json({ success: true, message: `Студент успешно создан` }, { status: 200 });
+    } catch (error) {
+        console.error("Ошибка работы API", error);
+        const errorMessage =
+            error instanceof Error ? error.message : "Неизвестная ошибка сервера";
+
+        return NextResponse.json(
+            {
+                success: false,
+                message: "Внутренняя ошибка сервера",
+                error: {
+                    message: errorMessage,
+                    code: "SERVER_ERROR",
+                },
+            },
+            { status: 500 }
+        );
+    }
+}
