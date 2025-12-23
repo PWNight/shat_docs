85b0b97d0ddc1ebcd0339251a2b8e17cc7865133
PWNight <rodionvarzymov@outlook.com>
2025-12-23 14:13:58 +0300

    Добавил API получения списка пользователей

---
+++ app/api/v1/users/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
+import { query } from "@/utils/mysql";
+import { getSession } from "@/utils/session";
+
+export async function GET(request: NextRequest) {
+    try {
+        const userData = await getSession();
+        if (!userData) {
+            return NextResponse.json({ success: false, message: "Необходима авторизация" }, { status: 401 });
+        }
+
+        const users = await query('SELECT id, email FROM users');
+
+        return NextResponse.json({ success: true, data: users }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ success: false, message: "Ошибка сервера" }, { status: 500 });
+    }
+}
