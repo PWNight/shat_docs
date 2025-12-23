bd7421eb2bf71520e3450a262bd6086d93cde26d
PWNight <rodionvarzymov@outlook.com>
2025-12-23 10:59:15 +0300

    Добавил API для получения студентов в группе

---
+++ app/api/v1/groups/[id]/students/route.ts
@@ -0,0 +1,36 @@
+import {NextRequest, NextResponse} from "next/server";
+import {query} from "@/utils/mysql";
+import {getSession} from "@/utils/session";
+
+
+// GET STUDENTS BY GROUP ID
+export async function GET(request: NextRequest, {params}: { params: Promise<{ id: string }> }) {
+    try{
+        const userData = await getSession();
+        if (!userData){
+            return NextResponse.json({ success: false, message: "Для доступа требуется авторизоваться" }, {status:401})
+        }
+
+        const {id} = await params;
+        const students = await query(
+            'SELECT * FROM students WHERE fk_group = ?', [id]
+        );
+        return NextResponse.json({ success: true, data: students }, {status:200})
+    } catch (error) {
+        console.error("Ошибка работы API", error);
+        const errorMessage =
+            error instanceof Error ? error.message : "Неизвестная ошибка сервера";
+
+        return NextResponse.json(
+            {
+                success: false,
+                message: "Внутренняя ошибка сервера",
+                error: {
+                    message: errorMessage,
+                    code: "SERVER_ERROR",
+                },
+            },
+            { status: 500 }
+        );
+    }
+}
