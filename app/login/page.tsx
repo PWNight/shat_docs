e01a1472c5d85922868637e6e3374b4e101a3328
PWNight <rodionvarzymov@outlook.com>
2026-01-19 09:30:41 +0300

    Сделал форму входа меньше по высоте

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -127,7 +127,7 @@ function LoginForm() {
 
 export default function LoginPage() {
     return (
-        <div className="flex items-center justify-center h-screen">
+        <div className="flex items-center justify-center">
             <Suspense fallback={<Loader2 className="h-10 w-10 animate-spin text-blue-400" />}>
                 <LoginForm />
             </Suspense>
