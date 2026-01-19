78f5f213f506c3dc5ddafdb8162d24fae89afcd0
PWNight <rodionvarzymov@outlook.com>
2026-01-19 09:31:15 +0300

    Сделал форму регистрации меньше по высоте

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -139,7 +139,7 @@ function RegisterForm() {
 
 export default function RegisterPage() {
     return (
-        <div className="flex items-center justify-center h-screen">
+        <div className="flex items-center justify-center">
             <Suspense fallback={<Loader2 className="h-10 w-10 animate-spin text-blue-400" />}>
                 <RegisterForm />
             </Suspense>
