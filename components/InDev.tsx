10a1e0ee81586db207a7ca26abaa44e0870aa9f5
PWNight <rodionvarzymov@outlook.com>
2026-01-20 09:19:51 +0300

    Убрал отступы у InDev

--- components/InDev.tsx
+++ components/InDev.tsx
@@ -3,7 +3,7 @@ import {CodeXml} from "lucide-react";
 
 export default function InDev() {
     return (
-        <div className="bg-neutral-100 rounded-sm p-4 py-14 dark:bg-neutral-800 flex flex-col w-fit">
+        <div className="bg-neutral-100 rounded-sm dark:bg-neutral-800 flex flex-col w-fit">
             <CodeXml className='h-32 w-32'/>
             <h1 className='text-4xl'>В разработке</h1>
             <p className='text-2xl text-muted-foreground'>Мы активно работаем над выпуском данного функционала</p>
