a1c244d7d5752faa79b0081e8fac42f61e8cafd6
PWNight <rodionvarzymov@outlook.com>
2026-01-20 09:20:07 +0300

    Исправил положение InDev на страницах

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import InDev from "@/components/InDev";
 
 export default function MainPage(){
     return (
-        <div className="flex justify-center my-4">
+        <div className="flex justify-center h-full items-center">
             <InDev />
         </div>
     )
