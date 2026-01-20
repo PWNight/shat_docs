a013914af4c17e9c4726fcb95eb44d47686a3bdc
PWNight <rodionvarzymov@outlook.com>
2026-01-20 13:17:17 +0300

    Добавил отступ в макет ЛК, чтобы исключить повторения на других страницах

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -3,7 +3,7 @@ import {NavProfile} from "@/components/NavProfile";
 
 export default function ProfileLayout({ children }: PropsWithChildren) {
     return (
-            <div className="grid sm:grid-cols-[250px_1fr] gap-6 lg:w-[95%] w-full mx-auto">
+            <div className="grid sm:grid-cols-[250px_1fr] gap-6 lg:w-[95%] w-full mx-auto mb-4 mt-4">
             <NavProfile/>
             {children}
         </div>
