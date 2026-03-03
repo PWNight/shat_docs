81120450d62375bab00263739d2237174ff3a09b
PWNight <rodionvarzymov@outlook.com>
2026-03-03 09:11:48 +0300

    Переработал layout кабинета

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -1,11 +1,13 @@
 import { PropsWithChildren } from "react";
-import {NavProfile} from "@/components/NavProfile";
+import { NavProfile } from "@/components/NavProfile";
 
 export default function ProfileLayout({ children }: PropsWithChildren) {
     return (
-            <div className="grid sm:grid-cols-[250px_1fr] gap-6 lg:w-[95%] w-full mx-auto mb-4 mt-4">
-            <NavProfile/>
-            {children}
+        <div className="grid sm:grid-cols-[250px_minmax(0,1fr)] gap-6 lg:px-4 w-full mx-auto mb-4 mt-4 overflow-hidden">
+            <NavProfile />
+            <main className="min-w-0">
+                {children}
+            </main>
         </div>
     );
 }
