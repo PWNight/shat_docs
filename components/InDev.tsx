b7cf91f64025ec2911e2d13a7378f71d43c0cbd1
PWNight <rodionvarzymov@outlook.com>
2026-03-15 09:52:37 +0300

    Адаптировал компонент ИнДев под темную тему

--- components/InDev.tsx
+++ components/InDev.tsx
@@ -2,7 +2,7 @@ import { CodeXml } from "lucide-react";
 
 export default function InDev() {
     return (
-        <div className="bg-white dark:bg-neutral-800 rounded-xl shadow-md p-8 py-9 flex flex-col items-center transition-all duration-300 hover:shadow-lg">
+        <div className="bg-white dark:bg-card rounded-xl shadow-md p-8 py-9 flex flex-col items-center transition-all duration-300 hover:shadow-lg">
             <CodeXml className="h-24 w-24 text-blue-500 dark:text-blue-400 mb-6" />
             <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100 mb-4">В разработке</h1>
             <p className="text-lg text-gray-600 dark:text-gray-400 text-center">
