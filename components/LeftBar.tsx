49e87c721c71a62b7eb56c6a3f6dbcedb21f0c5f
PWNight <rodionvarzymov@outlook.com>
2026-03-17 12:40:32 +0300

    Добавил навигацию по базе знаний

--- components/LeftBar.tsx
+++ components/LeftBar.tsx
@@ -7,10 +7,26 @@ import {
   SheetTrigger,
 } from "@/components/ui/Sheet";
 import { Logo, NavMenu } from "./NavMain";
-import { Button } from "./ui/Button";
 import { AlignLeftIcon } from "lucide-react";
 import { FooterButtons } from "./Footer";
 import { DialogTitle } from "./ui/Dialog";
+import { Button } from "./ui/Button";
+import WikiMenu from "@/components/wiki/wiki-menu";
+
+export function Leftbar() {
+  return (
+      <aside className="md:flex hidden w-[260px] flex-col overflow-y-auto">
+        <ScrollArea className="py-6 text-base">
+          <h2 className="text-xs font-semibold text-muted-foreground mb-5 uppercase tracking-wider">
+            Содержание
+          </h2>
+          <div className="flex flex-col gap-2">
+            <WikiMenu />
+          </div>
+        </ScrollArea>
+      </aside>
+  );
+}
 
 export function SheetLeftbar() {
   return (
@@ -43,6 +59,9 @@ export function SheetLeftbar() {
                 Навигация
               </h1>
               <NavMenu isSheet /> {/* Уже использует новый стиль выделения */}
+            </div>
+            <div className="flex flex-col gap-4 px-6 py-4">
+              <WikiMenu isSheet />
             </div>
             <div className="flex flex-col gap-3 px-6 py-4 mt-auto border-t border-muted/20">
               <h2 className="text-sm font-semibold text-muted-foreground uppercase tracking-wide">
