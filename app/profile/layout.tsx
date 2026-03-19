363e907efedc9b089df9c8c78e582da438200940
PWNight <rodionvarzymov@gmail.com>
2026-03-19 17:16:54 +0300

    Добавил отступ на мобильной версии layout ЛК

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -3,7 +3,7 @@ import { NavProfile } from "@/components/NavProfile";
 
 export default function ProfileLayout({ children }: PropsWithChildren) {
     return (
-        <div className="flex flex-col lg:grid lg:grid-cols-[250px_minmax(0,1fr)] gap-4 sm:gap-6 sm:px-4 w-full mx-auto my-4 overflow-hidden">
+        <div className="flex flex-col lg:grid lg:grid-cols-[250px_minmax(0,1fr)] gap-4 sm:gap-6 sm:px-4 px-2 w-full mx-auto my-4 overflow-hidden">
             <aside className="w-full lg:w-62.5">
                 <NavProfile />
             </aside>
