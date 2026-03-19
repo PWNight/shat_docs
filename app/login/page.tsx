ccd4b9275fb3704b486a38ce5ec8bbe6d7522d08
PWNight <rodionvarzymov@gmail.com>
2026-03-19 17:15:23 +0300

    Выровнял форму входа и регистрации по центру экрана

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -134,7 +134,7 @@ function LoginForm() {
 
 export default function LoginPage() {
     return (
-        <div className="flex items-center justify-center h-screen">
+        <div className="flex flex-1 flex-col items-center justify-center">
             <Suspense fallback={<Loader2 className="h-10 w-10 animate-spin text-blue-400" />}>
                 <LoginForm />
             </Suspense>
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -146,7 +146,7 @@ function RegisterForm() {
 
 export default function RegisterPage() {
     return (
-        <div className="flex items-center justify-center h-screen">
+        <div className="flex flex-1 flex-col items-center justify-center">
             <Suspense fallback={<Loader2 className="h-10 w-10 animate-spin text-blue-400" />}>
                 <RegisterForm />
             </Suspense>
