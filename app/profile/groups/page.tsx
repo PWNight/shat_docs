eaecba513a76a71938392b62253598030775605d
PWNight <rodionvarzymov@outlook.com>
2026-03-22 12:15:45 +0300

    Добавил анимацию появления для списка групп

--- app/profile/groups/page.tsx
+++ app/profile/groups/page.tsx
@@ -132,7 +132,7 @@ export default function ProfileGroups() {
     }
 
     return (
-        <div>
+        <div className={'animate-in fade-in duration-500'}>
             {notify.message && (
                 <ErrorMessage
                     message={notify.message}
