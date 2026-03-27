cc17c331c42ad1841e0a2c72ddb0aaa5c18904b7
PWNight <rodionvarzymov@outlook.com>
2026-03-27 21:00:27 +0300

    Обновил логику адаптации навигации по ЛК

--- components/NavProfile.tsx
+++ components/NavProfile.tsx
@@ -13,16 +13,16 @@ export function NavProfile() {
                 Навигация
             </h1>
 
-            <nav className="mt-4 space-y-1.5">
-                {navItems.map(({ href, label, icon: Icon }) => (
+            <nav className="mt-4 space-y-1.5 flex sm:flex-col flex-row">
+                {navItems.map(({href, label, icon: Icon}) => (
                     <Anchor
                         key={href}
                         href={href}
                         absolute
-                        className="group flex items-center gap-2 sm:gap-3 w-full px-2 sm:px-4 py-2.5 rounded-lg text-foreground hover:bg-blue-600 hover:text-white transition-all duration-200 font-medium text-base overflow-hidden"
+                        className="group flex items-center justify-center sm:justify-start gap-2 sm:gap-3 w-full px-2 py-2.5 rounded-lg text-foreground hover:bg-blue-600 hover:text-white transition-all duration-200 font-medium text-base overflow-hidden"
                         activeClassName="bg-blue-600 text-white shadow-md"
                     >
-                        <Icon className="w-5 h-5 shrink-0" />
+                        <Icon className="w-5 h-5 shrink-0"/>
                         <span className="truncate">{label}</span>
                     </Anchor>
                 ))}
