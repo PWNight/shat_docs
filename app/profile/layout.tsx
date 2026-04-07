01478bb8747bb32561581610844187d683292bda
PWNight <rodionvarzymov@outlook.com>
2026-04-07 09:35:33 +0300

    Убрал навигацию из layout

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -1,12 +1,8 @@
 import { PropsWithChildren } from "react";
-import { NavProfile } from "@/components/NavProfile";
 
 export default function ProfileLayout({ children }: PropsWithChildren) {
     return (
-        <div className="flex flex-col lg:grid lg:grid-cols-[250px_minmax(0,1fr)] gap-4 sm:gap-6 px-4 w-full mx-auto my-4 overflow-hidden">
-            <aside className="w-full lg:w-62.5">
-                <NavProfile />
-            </aside>
+        <div className="px-4 w-full mx-auto my-4 overflow-hidden">
             <main className="min-w-0 w-full">
                 {children}
             </main>
