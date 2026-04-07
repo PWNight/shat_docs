d3f688319018c02fe5f35b06af8b32899d25ce5a
PWNight <rodionvarzymov@outlook.com>
2026-04-07 13:52:04 +0300

    Добавил краткую документацию по приложению

--- contents/routes.ts
+++ contents/routes.ts
@@ -10,4 +10,20 @@ export const ROUTES : EachRoute[] = [
     title: "Оглавление",
     href: "/",
   },
+  {
+    title: "Быстрый старт",
+    href: "/quick-start",
+  },
+  {
+    title: "Руководство по приложению",
+    href: "/app-guide",
+  },
+  {
+    title: "API v1 vs v2",
+    href: "/api-v1-v2",
+  },
+  {
+    title: "Версии v0.X vs v1.X",
+    href: "/versions-v0-v1",
+  },
 ];
