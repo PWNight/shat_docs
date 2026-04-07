5d2ea2888555054a35b8e90e532b80a575688363
PWNight <rodionvarzymov@outlook.com>
2026-04-07 12:53:14 +0300

    Оптимизировал код not found

--- app/api/[...not_found]/route.ts
+++ app/api/[...not_found]/route.ts
@@ -1,22 +1,11 @@
+import { notFound } from "@/utils/api";
 import { NextResponse } from "next/server";
 export async function GET() {
-    return NextResponse.json({
-        success: false,
-        message: "Ресурс не найден",
-        code: "NOT_FOUND"
-    }, { status: 404 });
+    return notFound()
 }
 export async function POST() {
-    return NextResponse.json({
-        success: false,
-        message: "Ресурс не найден",
-        code: "NOT_FOUND"
-    }, { status: 404 });
+    return notFound()
 }
 export async function PATCH() {
-    return NextResponse.json({
-        success: false,
-        message: "Ресурс не найден",
-        code: "NOT_FOUND"
-    }, { status: 404 });
+    return notFound()
 }
