17ae27b258f629f66dc708ece0b8c2ca4a76f3be
PWNight <rodiongosh@gmail.com>
2026-05-18 13:30:47 +0300

    Вынес стандартные ответы по AUTH в файл

---
+++ utils/auth-messages.ts
@@ -0,0 +1,7 @@
+export const INVALID_CREDENTIALS_MESSAGE = "Неверный email или пароль";
+
+export const REGISTRATION_PENDING_MESSAGE =
+    "Ваша регистрация ожидает подтверждения администратором";
+
+export const REGISTRATION_SUCCESS_MESSAGE =
+    "Заявка на регистрацию отправлена. Ожидайте подтверждения администратором.";
